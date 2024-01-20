3dca982896b64a1e5db5a2e9658773368f69be98
Sagar <sagarchakrawarti24@gmail.com>
2024-01-20 22:25:51 +0530

    upgrade efficiency by using event delegation

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ form.addEventListener("submit", function (event) {
   list.innerHTML = taskInput.value;
   const removeBtn = document.createElement("button");
   removeBtn.innerHTML = `<img class="closeBtn" src="close.png"/>`;
-  removeBtn.addEventListener("click", function () {
-    list.remove();
-  });
+
   list.appendChild(removeBtn);
   taskList.appendChild(list);
+});
+
+taskList.addEventListener("click", function (event) {
+  if (event.target.tagName == "BUTTON") {
+    const targetList = this.querySelector(".task");
+    targetList.remove();
+  }
 });
